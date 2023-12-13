8d4b42f6995fa03e71f20a5a45ec79c75d8d4d4b
Dexter Balino <Dexter.Balino@edu.sait.ca>
2023-12-12 22:16:36 -0700

    added classes

---
+++ Classes/Database.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Maui.Controls;
+using Oracle.ManagedDataAccess.Client;
+
+namespace RestaurantManager.Classes
+{
+    internal class Database
+    {
+        private string connectionString = "User Id=your_username;Password=your_password;Data Source=your_datasource;"; //insert connection details here;
+        private string query = ""; // insert queries here;
+        public List<Reservation> reservations = new List<Reservation>();
+
+
+        public void ReadDatabase()
+        {
+            using (OracleConnection connection = new OracleConnection(connectionString))
+            {
+                connection.Open();
+
+                using (OracleCommand command = new OracleCommand(query, connection))
+                {
+                    this.FillLists(command);
+                }
+            }
+        }
+
+        public void FillLists(OracleCommand command) 
+        {
+            using (OracleDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine($"{reader["Column1"]}, {reader["Column2"]}, {reader["Column3"]}");
+
+                    // Replace Column1, Column2, Column3 with actual column names from your table.
+                }
+            }
+        }
+    }
+}
---
+++ Classes/Reservation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManager.Classes
+{
+    public class Reservation
+    {
+        public string ReservationCode { get; set; }
+        public string CustomerName { get; set; }
+        public string ReservationTime { get; set; }
+        public string GuestAmount { get; set; }
+        public string ServerName { get; set; }
+        public string TableNumber { get; set; }
+        public string Status { get; set; }
+
+        public Reservation(string code, string customer, string time, string guests, string server, string table, string status)
+        {
+            this.ReservationCode = code;
+            this.CustomerName = customer;
+            this.ReservationTime = time;
+            this.GuestAmount = guests;
+            this.ServerName = server;
+            this.TableNumber = table;
+            this.Status = status;
+        }
+
+    }
+}
